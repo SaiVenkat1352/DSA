ea88bb13dbee69be4d970cce3cc98c12fb6294d5
SaiVenkat1352 <156398185+SaiVenkat1352@users.noreply.github.com>
2024-02-26 15:12:29 +0530

    Time: 4 ms (71.43%), Space: 44 MB (95.20%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,35 +1,36 @@
+
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0)return null;
-        ListNode res=lists[0];
-        for(int i=1;i<lists.length;i++)
-        {
-            res=merge(res,lists[i]);
+        if (lists == null || lists.length == 0) return null;
+
+        // Define a priority queue to keep track of the smallest elements
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        // Add the heads of all the lists to the minHeap
+        for (ListNode list : lists) {
+            if (list != null) {
+                minHeap.offer(list);
+            }
         }
-        return res;
-    }
-    public ListNode merge(ListNode head1,ListNode head2)
-    {
+
         ListNode dummy = new ListNode();
         ListNode tail = dummy;
 
-        while(head1 != null && head2 != null) {
-            if (head1.val <= head2.val) {
-                tail.next = head1;
-                 head1=head1.next;
-            }
-            else {
-              tail.next = head2;
-              head2 = head2.next;
-            }
+        // Continue until all lists are merged
+        while (!minHeap.isEmpty()) {
+            // Get the smallest node from the minHeap
+            ListNode smallest = minHeap.poll();
+
+            // Add the smallest node to the merged list
+            tail.next = smallest;
             tail = tail.next;
+
+            // Move the pointer of the list from which the smallest node was extracted
+            if (smallest.next != null) {
+                minHeap.offer(smallest.next);
+            }
         }
-        if (head1 != null)tail.next = head1;
-       else tail.next = head2;
-       return dummy.next;
+
+        return dummy.next;
     }
-    
-   
-    
-      
 }
