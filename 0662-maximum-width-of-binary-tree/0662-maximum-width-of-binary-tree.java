d05334404322e8bc0adb90fae486a21006c6ab67
SaiVenkat1352 <156398185+SaiVenkat1352@users.noreply.github.com>
2024-02-29 08:31:03 +0530

    Time: 1 ms (98.76%), Space: 43 MB (86.48%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -13,45 +13,44 @@
  *     }
  * }
  */
-import java.util.*;
 
-class Pair {
+
+class Pair{
     int ind;
     TreeNode root;
-
-    Pair(int ind, TreeNode root) {
-        this.ind = ind;
-        this.root = root;
+    Pair(int ind,TreeNode root)
+    {
+        this.ind=ind;
+        this.root=root;
     }
 }
-
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
-
-        Queue<Pair> ds = new LinkedList<>();
-        ds.add(new Pair(1, root));
-        int maxWidth = 0;
-
-        while (!ds.isEmpty()) {
-            int size = ds.size();
-            int leftMostIndex = ds.peek().ind; // Leftmost index of current level
-            int rightMostIndex = leftMostIndex; // Rightmost index of current level
-
-            for (int i = 0; i < size; i++) {
-                Pair pair = ds.poll();
-                int ind = pair.ind;
-                TreeNode node = pair.root;
-                rightMostIndex = ind; // Update the rightmost index as we proceed
-
-                if (node.left != null) ds.add(new Pair(2 * ind, node.left));
-                if (node.right != null) ds.add(new Pair(2 * ind + 1, node.right));
+        if(root==null)return 0;
+        Queue<Pair> ds=new LinkedList<>();
+        ds.add(new Pair(1,root));
+        int maxi=0;
+        
+        while(ds.size()>0)
+        {
+            int dsSize=ds.size();
+            int leftMostIndex=0;
+            int rightMostIndex=0;
+            for(int i=0;i<dsSize;i++)
+            {
+                Pair pair=ds.poll();
+                int ind=pair.ind;
+                TreeNode node=pair.root;
+                if(i==0)leftMostIndex=ind;
+                if(i==dsSize-1)rightMostIndex=ind;
+                if(node.left!=null)ds.add(new Pair(2*ind-1,node.left));
+                if(node.right!=null)ds.add(new Pair(2*ind,node.right));
+                
+                
             }
-
-            // Update the maxWidth
-            maxWidth = Math.max(maxWidth, rightMostIndex - leftMostIndex + 1);
+            maxi=Math.max(rightMostIndex-leftMostIndex+1,maxi);
         }
-
-        return maxWidth;
+        return maxi;
+        
     }
-}
+} 
