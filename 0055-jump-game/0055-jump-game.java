3235eee0b30376df43ff71cadcb8cbc3522fc2c9
SaiVenkat1352 <156398185+SaiVenkat1352@users.noreply.github.com>
2025-01-06 10:43:32 +0530

    Time: 293 ms (10.04%), Space: 46 MB (26.53%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,17 +1,34 @@
 class Solution {
     public boolean canJump(int[] nums) {
-      int maxIndex=0;
-      for(int i=0;i<nums.length-1;i++)
-      {
-          if(i>maxIndex)return false;
-          if(maxIndex<nums[i]+i)
-          {
-              maxIndex=nums[i]+i;
-          }
-          
-      }
-      if(maxIndex<nums.length-1)return false;
-      return true;
-        
+        int n = nums.length;
+        Boolean[] memo = new Boolean[n]; // Memoization array to store results for each index
+        return helper(nums, 0, memo);
+    }
+
+    public static boolean helper(int[] nums, int ind, Boolean[] memo) {
+        // Base case: if the current index reaches or exceeds the last index
+        if (ind >= nums.length - 1) {
+            return true;
+        }
+
+        // Check if this subproblem is already solved
+        if (memo[ind] != null) {
+            return memo[ind];
+        }
+
+        // Get the maximum position you can jump to from the current index
+        int maxJump = nums[ind] + ind;
+
+        // Try every possible jump from the current index
+        for (int i = ind + 1; i <= maxJump && i < nums.length; i++) {
+            if (helper(nums, i, memo)) {
+                memo[ind] = true; // Store result for the current index
+                return true;
+            }
+        }
+
+        // If no jump leads to the end, store false in the memo array
+        memo[ind] = false;
+        return false;
     }
 }
