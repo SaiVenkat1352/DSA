967dfcc5428bb7c5a9d32b5c7cbf5a0e78b341cb
SaiVenkat1352 <156398185+SaiVenkat1352@users.noreply.github.com>
2025-03-11 08:51:31 +0530

    Time: 1 ms (17.19%), Space: 41.9 MB (41.66%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.java
@@ -16,35 +16,39 @@
 class Solution {
     public TreeNode bstFromPreorder(int[] preorder) {
         int n=preorder.length;
-        TreeNode root=new TreeNode(preorder[0]);
-        for(int i=1;i<n;i++)
-        {
-            TreeNode temp=root;
-            while(temp!=null)
-            {
-                if(preorder[i]<temp.val)
-                {
-                    if(temp.left!=null)temp=temp.left;
-                    else{
-                        temp.left=new TreeNode(preorder[i]);
-                        break;
-                    }
-                }
-                else{
-                    if(temp.right!=null)temp=temp.right;
-                    else{
-                        temp.right=new TreeNode(preorder[i]);
-                        break;
-                    }
-
-                }
-            }
-
-        }
-        return root;
-           
+        int inorder[]=new int[n];
+        int k=0;
         
+        for(int ele:preorder)
+        {
             
+            inorder[k]=ele;
+            k++;
+        }
+        Arrays.sort(inorder);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(inorder[i], i);
+        }
+        TreeNode res=helper(preorder,0,n-1,inorder,0,n-1,map);
+        return res;
+            
+    }
+    public TreeNode helper(int preorder[],int preStart,int preEnd,int inorder[],int inStart,int inEnd,Map<Integer,Integer> map)
+    {
+        //base case
+        if(preStart>preEnd || inStart>inEnd)return null;
+
+
+        int root=preorder[preStart];
+        int inroot=map.get(root);
+        int numsLeft=inroot-inStart;
+        TreeNode node=new TreeNode(root);
+        node.left=helper(preorder,preStart+1,preStart+numsLeft,inorder,inStart,inroot-1,map);
+        node.right=helper(preorder,preStart+numsLeft+1,preEnd,inorder,inroot+1,inEnd,map);
+        return node;
+        
+
     }
     
     
